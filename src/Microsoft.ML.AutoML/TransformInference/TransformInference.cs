2a88197f105ad7ecab17ff634e14d56fe448ed05
Lynx1820 <dkeren@seas.upenn.edu>
2020-06-11 12:08:30 -0700

    concat feature columns addition

--- src/Microsoft.ML.AutoML/TransformInference/TransformInference.cs
+++ src/Microsoft.ML.AutoML/TransformInference/TransformInference.cs
@@ -406,7 +406,7 @@ namespace Microsoft.ML.AutoML
                 suggestedTransforms.AddRange(suggestions);
             }
 
-            if (task != TaskKind.Recommendation && task != TaskKind.Ranking)
+            if (task != TaskKind.Recommendation)
             {
                 var finalFeaturesConcatTransform = BuildFinalFeaturesConcatTransform(context, suggestedTransforms, intermediateCols);
                 if (finalFeaturesConcatTransform != null)
